84bdf462c657013244f9a11249aa7462c00c0450
tungnt29 <105925488+tungnt29@users.noreply.github.com>
2022-06-06 23:00:40 +0700

    Add files via upload

---
+++ IndexQuery_Nguyễn_Thanh_Tùng_20187353.sql
@@ -0,0 +1,12 @@
+﻿
+10. So sánh sử dụng và không sử dụng index:
+select * from HumanResources.Employee where LoginID ='adventure-works\jean0'
+
+11. Sử dụng clustered index:
+Select * from Person.Person where BusinessEntityID = '202'
+12. Sử dụng non-clustered index:
+Create index ix_person_firstname
+On Person.Person(FirstName)
+Select * from Person.Person where FirstName = 'Tawana'
+
+
